0cadedc688148842f8bfdf104a4e1f47de7d1e09
9831065 <najafizadeh21@gmail.com>
2021-07-17 10:34:44 +0430

    User sql added

---
+++ source/sql/table/entity/user.sql
@@ -0,0 +1,11 @@
+CREATE TABLE 'user' as (
+	username varchar(50),
+	password varchar(50) NOT NULL,
+	name varchar(50) NOT NULL,
+	email varchar(50) NOT NULL,
+	phonenumber varchar(50),
+	nationalID varchar(10) UNIQUE,
+	wallet int DEFAULT 0,
+	point int DEFAULT 0,
+	PRIMARY KEY (username)
+);
