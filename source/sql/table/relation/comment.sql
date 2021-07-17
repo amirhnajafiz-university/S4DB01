2553024f36a1fcb46ded347e1efdc514c647ac46
9831065 <najafizadeh21@gmail.com>
2021-07-17 10:52:29 +0430

    Add Comment

---
+++ source/sql/table/relation/comment.sql
@@ -0,0 +1,9 @@
+CREATE TABLE 'comment' as (
+	rate int,
+	user_comment varchar(1024),
+	username varchar(50),
+	movie_id int,
+	PRIMARY KEY (username, movie_id),
+	FOREIGN KEY username REFERENCES ('user'.username),
+	FOREIGN KEY movie_id REFERENCES ('movie'.movie_id)
+);
