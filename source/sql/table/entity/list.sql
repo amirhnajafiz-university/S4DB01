756c27d3c5ac113b5f56e116ae4891fd3ad9023e
9831065 <najafizadeh21@gmail.com>
2021-07-17 10:44:43 +0430

    List and Tag SQL added

---
+++ source/sql/table/entity/list.sql
@@ -0,0 +1,6 @@
+CREATE TABLE 'list' as (
+	list_id int,
+	name varchar(1024) NOT NULL,
+	description varchar(1024),
+	PRIMARY KEY (list_id)
+);
---
+++ source/sql/table/entity/tag.sql
@@ -0,0 +1,5 @@
+CREATE TABLE 'tag' as (
+	tag_id int,
+	name varchar(50) NOT NULL,
+	PRIMARY KEY (tag_id)
+);
